e45bcc8dfddd195f463deec30e15f63782747a5e
Gabriel <gabrieldcarmo1@gmail.com>
2026-05-25 12:09:24 -0300

    3 - Fazendo as caixinhas de seleção funcionarem

--- dados.php
+++ dados.php
@@ -1,23 +1,23 @@
 <?php
 $tipos = [
-    "Bolos" => "Bolos",
-    "Doces" => "Doces",
-    "Salgados" => "Salgado",
+    "bolos" => "Bolos",
+    "doces" => "Doces",
+    "salgados" => "Salgado"
 ];
 $sabor = [
-    "Bolos" => ["Chocolate", "Cenoura", "Prestígio", "Morango", "Limão", "Leite Ninho"],
-    "Salgados" => ["Coxinha", "Kibe", "Risoles", "Bolinha de Queijo", "Enroladinho"],
-    "Doces" => ["Brigadeiro", "Beijinho", "Cajuzinho", "Camafeu"]
+    "bolos" => ["Chocolate", "Cenoura", "Prestígio", "Morango", "Limão", "Leite Ninho"],
+    "salgados" => ["Coxinha", "Kibe", "Risoles", "Bolinha de Queijo", "Enroladinho"],
+    "doces" => ["Brigadeiro", "Beijinho", "Cajuzinho", "Camafeu"]
 ];
 
 $quantidade = [
-    "Doces" => [50,100, 200, 300, 400, 500 ],
-    "Salgados" => [50,100, 200, 300, 400, 500 ],
-    "Bolos" => [1, 2, 3, 4, 5],
+    "doces" => ["50", "100", "200", "300", "400", "500" ],
+    "salgados" => ["50", "100", "200", "300", "400", "500" ],
+    "bolos" => ["1", "2", "3", "4", "5"]
 ];
 
-produtos = [
-    Bolos => [
+$produtos = [
+    "bolos" => [
         [
             "sabor" => "Chocolate",
             "preco" => 45,
@@ -35,19 +35,66 @@ produtos = [
         ],
         [
             "sabor" => "Morango",
-            "preco" => "",
-            "descricao" => ""
+            "preco" => 50,
+            "descricao" => "Massa branca com recheio de morango"
         ],
         [
-            "sabor" => "",
-            "preco" => "",
-            "descricao" => ""
+            "sabor" => "Limão",
+            "preco" => 42,
+            "descricao" => "Massa branca com mousse de limão"
         ],
         [
-            "sabor" => "",
-            "preco" => "",
-            "descricao" => ""
+            "sabor" => "Leite Ninho",
+            "preco" => 60,
+            "descricao" => "Massa branca com creme de leite ninho"
+        ]
+    ],
+    "salgados" => [
+        [
+            "sabor" => "Coxinha",
+            "preco" => 1.20,
+            "descricao" => "Coxinha de frango tradicional"
         ],
-    ]
-]
+        [
+            "sabor" => "Kibe",
+            "preco" => 1.50,
+            "descricao" => "Kibe frito recheado"
+        ],
+        [
+            "sabor" => "Risole",
+            "preco" => 1.30,
+            "descricao" => "Risole de presunto e queijo"
+        ],
+        [
+            "sabor" => "Bolinha de Queijo",
+            "preco" => 1.10,
+            "descricao" => "Massa leve com queijo"
+        ],
+        [
+            "sabor" => "Enroladinho",
+            "preco" => 1.40,
+            "descricao" => "Enroladinho de salsicha"
+        ]
+    ],
+    "doces" => [
+        "sabor" => "Brigadeiro",
+        "preco" => 0.8,
+        "descricao" => "Tradicional brigadeiro de chocolate",
+    ],
+    [
+        "sabor" => "Beijinho",
+        "preco" => 0.8,
+        "descricao" => "Doce de coco com leite condensado",
+    ],
+    [
+        "sabor" => "Cajuzinho",
+        "preco" =>0.9,
+        "descricao" => "Doce de amendoim",
+    ],
+    [
+        "sabor" => "Camafeu",
+        "preco" => 1.5,
+        "descricao" => "Nozes com cobertura especial",
+    ],
+];
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     include "dados.php";
     $tiposSelecionado = $_POST['tipos'] ?? "" ;
     $saborSelecionado = $_POST['sabor'] ?? "";
+    $qtdSelecionada = $_POST['qtd'] ?? "";
 ?>
 
 <!DOCTYPE html>
@@ -21,48 +22,39 @@
 <div class="container">
     <h2>Monte seu Pedido</h2>
 
-    <form mothod ="POST">
+    <form method ="post">
 
         <!-- SELECT 1 - Tipo de Produto -->
-        <label for="tipo">O que deseja comprar?</label>
-        <select id="tipos" name="tipos"  onchange="this.form.submit()">
+        <label for="tipos">O que deseja comprar?</label>
+        <select id="tipos" name="tipos" onchange="this.form.submit()">
             <option value="">Selecione...</option>
             <?php foreach ($tipos as $key => $value): ?>
                 <option value="<?= $key ?>" <?= ($tiposSelecionado === $key) ? "selected" : "" ?>><?= $value ?> </option>
             <?php endforeach; ?>
+            
         </select>
 
         <!-- SELECT 2 - Opções do Produto -->
-         <?php if ($tiposSelecionado && isset($sabor[$tiposSelecionado])): ?>
+        <?php if ($tiposSelecionado && isset($sabor[$tiposSelecionado])): ?>
         <label for="sabor">Escolha o sabor / tipo:</label>
         <select name="sabor" id="sabor" onchange="this.form.submit()">
             <option value="">Selecione...</option>
                 <?php foreach ($sabor [$tiposSelecionado] as $opcao): ?>
-                    <option value="<?= $opcao ?>" <?= ($saborSelecionado === $opcao) ? "selected " : "" ?>> <?= $opcao?></option>
+                    <option value="<?= $opcao ?>" <?= ($saborSelecionado === $opcao) ? "selected" : "" ?>> <?= $opcao?></option>
                 <?php endforeach; ?> 
         </select>
         <?php endif; ?>
 
         <!-- SELECT 3 - Quantidade -->
-        <label for="quantidade">Quantidade:</label>
-        <select id="quantidade" name="quantidade">
+        <?php if ($saborSelecionado && isset($quantidade[$tiposSelecionado])): ?>
+        <label for="qtd">Escolha a quantidade:</label>
+        <select name="qtd" id="qtd" onchange="this.form.submit()">
             <option value="">Selecione...</option>
-
-            <!-- QUANTIDADE EM KG (para bolos) -->
-            <option value="1kg">1 Kg</option>
-            <option value="2kg">2 Kg</option>
-            <option value="3kg">3 Kg</option>
-            <option value="4kg">4 Kg</option>
-            <option value="5kg">5 Kg</option>
-
-            <!-- QUANTIDADE EM UNIDADES (para salgados e doces) -->
-            <option value="50">50 unidades</option>
-            <option value="100">100 unidades</option>
-            <option value="200">200 unidades</option>
-            <option value="300">300 unidades</option>
-            <option value="400">400 unidades</option>
-            <option value="500">500 unidades</option>
+                <?php foreach ($quantidade [$tiposSelecionado] as $qtd): ?>
+                    <option value="<?= $qtd ?>" <?= ($qtdSelecionada === $qtd) ? "selected" : "" ?>><?= $qtd?><?=  ($tiposSelecionado == "bolos") ? "Kg" : " Unidades" ?></option>
+                <?php endforeach; ?> 
         </select>
+        <?php endif; ?>
 
     </form>
 </div>
